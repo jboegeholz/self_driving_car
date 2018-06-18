6432e160939cf2aa2d61bf8ed50784d6a3794e63
jobo8505 <boegeholz.joern@gmail.com>
2018-06-18 16:09:33 +0200

    add hough transform

--- lane_detection/grayscale_conversion/grayscale_conversion.py
+++ lane_detection/grayscale_conversion/grayscale_conversion.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
-img = cv2.imread("dog.jpg")
+import numpy as np
+img = cv2.imread("lanes.jpg")
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 print(img.shape)
@@ -8,6 +9,12 @@ print(img.shape)
 #plt.show()
 
 
-edges = cv2.Canny(gray_image, 100, 10)
-plt.imshow(edges, cmap="gray", vmin=0, vmax=255)
+edges = cv2.Canny(gray_image, 200, 10)
+
+lines = cv2.HoughLinesP(edges , 1, np.pi/180, 100, 300, 10)
+for x in range(0, len(lines)):
+    for x1, y1, x2, y2 in lines[x]:
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+plt.imshow(img, cmap="gray", vmin=0, vmax=255)
 plt.show()
