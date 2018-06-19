3899151a63b91297e83b20a82c248f8ca6768151
jobo8505 <boegeholz.joern@gmail.com>
2018-06-19 13:32:05 +0200

    display all images in pygame window

--- lane_detection/grayscale_conversion/grayscale_conversion.py
+++ lane_detection/grayscale_conversion/grayscale_conversion.py
@@ -1,20 +1,46 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+import pygame
+import sys
+WHITE = (255, 255, 255)
+
+
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
 img = cv2.imread("lanes.jpg")
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-print(img.shape)
-#plt.imshow(gray_image, cmap="gray", vmin=0, vmax=255)
-#plt.show()
 
 
-edges = cv2.Canny(gray_image, 200, 10)
+edgy_image = cv2.Canny(gray_image, 200, 10)
 
-lines = cv2.HoughLinesP(edges , 1, np.pi/180, 100, 300, 10)
+lines = cv2.HoughLinesP(edgy_image, 1, np.pi/180, 100, 300, 10)
 for x in range(0, len(lines)):
     for x1, y1, x2, y2 in lines[x]:
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-plt.imshow(img, cmap="gray", vmin=0, vmax=255)
-plt.show()
+gray_scale_rgb = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2RGB)
+
+
+def cv2pygame(img):
+    img = np.rot90(img)
+    img = pygame.surfarray.make_surface(img)
+    return img
+
+
+
+
+try:
+    while True:
+        screen.blit(cv2pygame(rgb_img), (0, 0))
+        screen.blit(cv2pygame(gray_scale_rgb), (480, 0))
+        screen.blit(cv2pygame(edgy_image), (960, 0))
+        screen.blit(cv2pygame(img), (1440, 0))
+        pygame.display.flip()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+except KeyboardInterrupt:
+    cv2.destroyAllWindows()
